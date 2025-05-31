15a7bbef8bd738e48e29744230db7d721f10e863
XxSebasx <125697490+XxSebasx@users.noreply.github.com>
2025-05-31 18:05:54 +0200

    puertos

--- servidorweb/servidorweb-main/backend/index.js
+++ servidorweb/servidorweb-main/backend/index.js
@@ -16,7 +16,7 @@ const Pelicula = require('./models/pelicula'); // Importa el modelo Pelicula
 
 const cors = require('cors');
 app.use(cors({
-    origin: ['http://localhost:4200', 'http://192.168.1.146:4200']
+    origin: ['http://localhost:4200', 'http://192.168.1.146:4200'],// puertos
     credentials: true
 }));
 
